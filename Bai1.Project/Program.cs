df30d0e961fa4bb6654c231b74864a114e54f547
KieuTrungHieu <kieutrunghieu104@gmail.com>
2026-08-18 11:23:56 +0700

    Hello world

---
+++ Bai1.Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1.Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("hello");
+            Console.ReadKey(true);
+        }
+    }
+}
